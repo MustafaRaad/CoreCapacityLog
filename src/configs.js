c7ed43f902d2ecb4ca1ebdb3bf535c160ce126f4
Mustafa Raad <mustf.raad@gmail.com>
2022-12-27 15:58:29 +0300

    Xlsx fetches of data is done

--- src/configs.js
+++ src/configs.js
@@ -1,4 +1,4 @@
 export const Configs = {
-  appId: 'ENTER_YOUR_CLIENT_ID',
+  appId: '66931ad8-977d-4d5c-8f0d-adfc242e49a7',
   scope: 'user.read,files.readwrite'
 };
--- src/home/home.js
+++ src/home/home.js
@@ -11,42 +11,43 @@ class Home extends Component {
     this.state = {
       me: "",
       successMessage: "",
+      xlsxData: {},
       token: msft.access_token
     };
 
-    this.onWriteToExcel = this.onWriteToExcel.bind(this);
+    // this.onFetchExcelData = this.onFetchExcelData.bind(this);
     this.onLogout = this.onLogout.bind(this);
-  }
 
-  componentDidMount() {
+
+  }
+  async componentDidMount() {
     const { token } = this.state;
     axios.get(
       'https://graph.microsoft.com/v1.0/me?$select=displayName,mail,userPrincipalName',
-      { headers: { Authorization: `Bearer ${token}` }}
+      { headers: { Authorization: `Bearer ${token}` } }
     ).then(res => {
       const me = res.data;
       this.setState({ me });
     });
-  }
-
-  onWriteToExcel() {
-    const { token, me } = this.state;
-
-    const myEmailAddress = me.mail || me.userPrincipalName;
-    const values = [];
-
-    values.push([me.displayName, myEmailAddress]);
-
     axios
-      .post('https://graph.microsoft.com/v1.0/me/drive/root:/demo.xlsx:/workbook/tables/Table1/rows/add',
-        { index: null, values },
-        { headers: { Authorization: `Bearer ${token}` }}
+      .get("https://graph.microsoft.com/v1.0/drives/b!rv9qh9qe60ay10NPvtjGbxGy1-IwSUdCgPhiZyJfMXjt8D0-O3QQR5DYvhLdsgis/items/01A4OPHAHO5KQRHTRJPRA3MKJP4SXW2TEY/workbook/worksheets('Capacity tracking ')/usedRange",
+        // { index: null, values },
+        { headers: { Authorization: `Bearer ${token}` } }
       )
-      .then(res => { 
-                      console.log(res);
-                      const successMessage = "Successfully wrote your data to demo.xlsx!";
-                      this.setState ({ successMessage });
-                     })
+      .then(res => {
+        // console.log(res, "API Call");
+        const xlsxData = res.data;
+
+        // const successMessage = "Successfully wrote your data to demo.xlsx!";
+        this.setState({ xlsxData });
+        // const customHeadings = res.data.map(item => ({
+        //   "Article Id": item.id,
+        //   "Article Title": item.title
+        // }))
+
+        // console.log(customHeadings);
+        // setData(customHeadings)
+      })
       .catch(err => console.error(err));
   }
 
@@ -62,17 +63,69 @@ class Home extends Component {
     const myEmailAddress = me.mail || me.userPrincipalName;
 
     return (
-        <tr>
-          <td>{me.displayName}</td>
-          <td>{myEmailAddress}</td>
-        </tr>
+      <tr>
+        <td>{me.displayName}</td>
+        <td>{myEmailAddress}</td>
+      </tr>
     );
+  }
+  renderMe2() {
+    const { xlsxData } = this.state;
+    if (Object.keys(xlsxData).length > 0) {
+      const valuesData = xlsxData.text;
+      const valueTypes = xlsxData.valueTypes;
+      var lastColIndex = 0
+      // console.table(valuesData[1], "xlsxData renderMe2");
+      for (let index = valuesData[1].length; index > 0; index--) {
+        const element = valuesData[1][index];
+        if (element !== '' && element !== undefined) {
+          console.table(element, index);
+          this.lastColIndex = index;
+          break
+        }
+      }
+
+      // valuesData[1].forEach((element, index) => {
+      //   if (valueTypes[1][index] !== 'Empty') {
+      //     // console.table(element);
+      //     // this.lastCol.push(element);
+      //     console.table(element);
+      //   }
+      //   // console.log(element[element.length-10]);
+      // });
+      // console.log(valuesData, "renderMe2");
+      // console.log(xlsxData.valueTypes[0][0] == 'Empty' && xlsxData.valueTypes[0][1] == 'Empty' && xlsxData.valueTypes[0][2] == 'Empty');
+      return (
+        // {/* <td>{valuesData}</td> */ }
+        //   {
+        //   valuesData.forEach(element => {
+        //     console.log(element);
+        //   })
+        // }
+        // {if (xlsxData.valueTypes.element)}
+        <tbody>
+          {
+            valuesData.map((element, index) => {
+              return (
+                <tr key={index}>
+                  <td>{element[0]}</td>
+                  <td>{element[1]}</td>
+                  <td>{element[this.lastColIndex]}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      )
+    } else {
+      return "Loading"
+    }
+
   }
 
   render() {
     return (
-      <div>
-        <table>
+      <div className='container'>
+        <table className="table stripped">
           <thead>
             <tr>
               <th>Name</th>
@@ -83,10 +136,20 @@ class Home extends Component {
             {this.renderMe()}
           </tbody>
         </table>
+        <table className="table table-striped-columns ">
+          <thead>
+            <tr>
+              <th>Site</th>
+              <th>Supplier</th>
+              <th>Supplier</th>
+            </tr>
+          </thead>
+          {this.renderMe2()}
+        </table>
 
-        <button onClick={this.onWriteToExcel}>Write to Excel</button>
+        {/* <button onClick={this.onFetchExcelData}>Write to Excel</button> */}
         <button onClick={this.onLogout}>Logout</button>
-        <p>{this.state.successMessage}</p>
+        {/* <p>{this.state.successMessage}</p> */}
       </div>
     );
   }
